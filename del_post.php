b63441327b521053cc6713888ea6dfa0d31284b1
Kdkocian <krencent870@Gmail.com>
2019-04-29 21:03:34 -0500

    fix things

--- del_post.php
+++ del_post.php
@@ -3,7 +3,7 @@ session_start();
 include 'db.php';
 $db = new myfuncs();
 $conn = $db -> dbConnect();
-
-$sql = "DELETE FROM posts WHERE post_ID = post_ID";
+$id = mysqli_real_escape_string($conn, $_GET['pid']);
+$sql = "DELETE FROM posts WHERE post_ID = '$id'";
 mysqli_query($conn, $sql);
 header("Location: adminview.php");
