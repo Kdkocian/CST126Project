bd3287dff4480706dd51fce850be01b790e53e68
Kdkocian <krencent870@Gmail.com>
2019-04-29 22:17:17 -0500

    deleted all edit logic as it did not work and removed post before edit button was pressed

--- del_post.php
+++ del_post.php
@@ -3,7 +3,7 @@ session_start();
 include 'db.php';
 $db = new myfuncs();
 $conn = $db -> dbConnect();
-$id = mysqli_real_escape_string($conn, $_GET['pid']);
-$sql = "DELETE FROM posts WHERE post_ID = '$id'";
+$id = mysqli_real_escape_string($conn, $_GET['pid']); //gets pid that is then assigned to id
+$sql = "DELETE FROM posts WHERE post_ID = '$id'";   //delets post from posts database
 mysqli_query($conn, $sql);
 header("Location: adminview.php");
--- edit_post.php
+++ edit_post.php
@@ -1,2 +1,3 @@
 <?php 
+//WORK IN PROGRESS!!!!!!!!
 ?>
