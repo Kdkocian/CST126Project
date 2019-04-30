1ce81755427b6ae8277cd960060530f180856290
Kdkocian <krencent870@Gmail.com>
2019-04-29 21:29:38 -0500

    changing edit logic

--- edit_post.php
+++ edit_post.php
@@ -3,8 +3,19 @@ include 'db.php';
 $db = new myfuncs();
 $conn = $db -> dbConnect();
 
-$id = mysqli_real_escape_string($conn, $_GET['pid']);
-$sql = "UPDATE FROM posts WHERE post_ID = '$id'";
+//$id = mysqli_real_escape_string($conn, $_GET['pid']);
+
+if(isset($_POST['update'])){
+    $title = strip_tags($_POST['article_title']);
+    $content = strip_tags($_POST['post_content']);
+    
+    $title = mysqli_real_escape_string($conn, $title);
+    $content = mysqli_real_escape_string($conn, $content);
+$sql = "UPDATE posts SET article_title='$title', post_content='$content'";
+}
+
 mysqli_query($conn, $sql);
+
+
 header("Location: edit_post.php");
 ?>
