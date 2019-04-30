6969c0e01b23b11cab30dfce3c217b29e844a122
Kdkocian <krencent870@Gmail.com>
2019-04-29 21:52:49 -0500

    changing edit logic

--- edit_post.php
+++ edit_post.php
@@ -40,16 +40,15 @@ $res = mysqlI_quert($conn, $sql_get);
             if(mysqli_num_rows($res) > 0){
                 while ($row = mysqli_fetch_assoc($res)){
                     $title = $row['article_title'];
-                    $content = $row['content'];
+                    $content = $row['post_content'];
                     
                   echo  "<form action='edit_post.php?pid=$pid' method='post' enctype='multipart/form-data'>";
                   echo  "<input placeholder='Title' name='title' type='text' value='$title' autofocus size='48'><br /><br />";
                   echo  "<textarea placeholder='Content' name='content' rows='20' cols='50'>$content</textarea><br /?";
                     
                 }
-            }
-            
-            ?>
+            }           
+?>
                 <input name="update" type="submit" value="Update">	
 </form>
 </body>
