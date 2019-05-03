59bfc668cba404c7d30a415cb010cac132add845
Kyle Kocian <krencent870@gmail.com>
2019-05-03 17:43:37 -0400

    changed get statement in edit

--- edit_post.php
+++ edit_post.php
@@ -6,9 +6,9 @@ $conn = $db -> dbConnect();
 //$id = mysqli_real_escape_string($conn, $_GET['pid']);
 
 $pid = $_GET['pid'];
-if(isset($_POST['update'])){
-    $title = strip_tags($_POST['article_title']);
-    $content = strip_tags($_POST['post_content']);
+if(isset($_POST['Edit'])){
+    $title = ($_POST['article_title']);
+    $content = ($_POST['post_content']);
     
     $title = mysqli_real_escape_string($conn, $title);
     $content = mysqli_real_escape_string($conn, $content);
