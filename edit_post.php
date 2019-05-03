cf7793b689f9d53092e3a604b83b243c3e4ff414
Kyle Kocian <krencent870@gmail.com>
2019-05-03 17:29:26 -0400

    trying edit logic again

--- edit_post.php
+++ edit_post.php
@@ -1,3 +1,21 @@
 <?php 
 //WORK IN PROGRESS!!!!!!!!
-?>
+<?php
+session_start();
+include 'db.php';
+$db = new myfuncs();
+$conn = $db -> dbConnect();
+//$id = mysqli_real_escape_string($conn, $_GET['pid']);
+
+$pid = $_GET['pid'];
+if(isset($_POST['update'])){
+    $title = strip_tags($_POST['article_title']);
+    $content = strip_tags($_POST['post_content']);
+    
+    $title = mysqli_real_escape_string($conn, $title);
+    $content = mysqli_real_escape_string($conn, $content);
+    
+    $sql = "UPDATE posts SET article_title='$title', post_content='$content'";
+}
+mysqli_query($conn, $sql);
+header("Location: postview.php");
