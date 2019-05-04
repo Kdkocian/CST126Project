6b9310d1e2d0384417d926a72524048dc8fc1a2c
Kyle Kocian <krencent870@gmail.com>
2019-05-03 20:33:54 -0400

    added file to control edit

--- editpostcontroller.php
+++ editpostcontroller.php
@@ -7,8 +7,8 @@ $id = mysqli_escape_string($conn, $_POST['pid']);
 $title = mysqli_escape_string($conn, $_POST['title']);
 $content = mysqli_escape_string($conn, $_POST['content']);
 
-$sql = "UPDATE posts SET article_title = '$title' and post_content = '$content' WHERE post_ID = '$id'";
+$sql = "UPDATE posts SET article_title = '$title', post_content = '$content' WHERE post_ID = '$id'";
 
 mysqli_query($conn, $sql);
-//header("Location: postview.php");
+header("Location: postview.php");
 ?>
