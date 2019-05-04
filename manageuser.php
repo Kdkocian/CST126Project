2f9cd3f68175a7d2fe15961c6c6e31ed86cfacf0
Kyle Kocian <krencent870@gmail.com>
2019-05-03 23:34:17 -0400

    added change user to admin logic

---
+++ manageuser.php
@@ -0,0 +1,18 @@
+<?php
+include 'dp.php';
+$db = new myfuncs();
+$conn = $db ->dbConnect();
+
+$id = mysqli_real_escape_string($conn, $_GET['id']);
+$sql = "SELECT * FROM users WHERE ID = '$id'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_row($result);
+
+$adminstatus = false;
+$adminstatus = !$row['admin']; 
+
+$sql= "Update users SET admin = '$adminstatus' WHERE ID = '$id'";
+mysqli_query($conn, $sql);
+close($conn);
+header("Location: adminmanagement.php");
+?>
