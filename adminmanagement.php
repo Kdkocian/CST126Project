0ea945b0fa9cc3f66b0dc2b5f75e6f14c43dd488
Kyle Kocian <krencent870@gmail.com>
2019-05-03 21:55:12 -0400

    adding logic to admin management

--- adminmanagement.php
+++ adminmanagement.php
@@ -3,4 +3,19 @@ include_once("db.php");
 $db = new myfuncs();
 $conn = $db -> dbConnect();
 
-echo "hello world";
+/*$id = mysqli_escape_string($conn, $_GET['pid']);
+$uname = mysqli_escape_string($conn, $_GET['USERNAME']);*/
+
+    function getAllusers(){
+        $db = new myfuncs();
+        $conn = $db->dbConnect();
+        $sql = "SELECT ID, USERNAME FROM users";
+        $users = array();
+        $result = mysqli_query($conn, $sql);
+        
+        while ($row = mysqli_fetch_array($result)){
+            $users[] = array($row['ID'], $row['USERNAME']);
+        }
+        mysqli_close($conn);
+        return $users;
+    }
