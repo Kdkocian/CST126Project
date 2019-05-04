c02e812fbd84dd2563083a14c9be348874fe320f
Kyle Kocian <krencent870@gmail.com>
2019-05-04 19:18:38 -0400

    added form to admin management button

--- adminmanagement.php
+++ adminmanagement.php
@@ -8,16 +8,16 @@ $db = new myfuncs();
 <body>
 <table>
 <tr><th>ID</th><th>Username</th><th>Delete/Change User</th></tr>
-"<form action = 'manageuser.php' method = 'GET'>"
 <?php
 echo "<form action = 'manageuser.php' method = 'GET'>";
 $user = $db->getAllusers();
 for($id = 0;$id < count($user);$id++){
+    echo "<form action = 'manageuser.php' method = 'GET'>";
     echo "<tr><td>".$user[$id][0]."</td><td>".$user[$id][1]."</td>";
     echo '<td><input type="button" value="Delete" onclick="window.location.href=\'manageuser.php?id='.$user[$id][0].'\'/">';
     echo '<input type="button" value="Make Admin" onclick="window.location.href=\'manageuser.php?id='.$user[$id][0].'\'/"></td></tr>';
+    echo "</form>";
 }
-echo "</form>";
 ?>
 </table>
 </body>
