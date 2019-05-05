391e6d560c2dd17747033d6a1e2cdef800f0fc6d
Kyle Kocian <krencent870@gmail.com>
2019-05-05 14:03:27 -0400

    added deluser php file as if statements are not working in mmy version of my  IDE

---
+++ deluser.php
@@ -0,0 +1,13 @@
+<?php
+include 'db.php';
+$db = new myfuncs();
+$conn = $db -> dbConnect();
+
+$id = mysqli_real_escape_string($conn, $_POST['ID']);
+
+$sql = "DELETE USERNAME SET users WHERE ID = '$id'";
+mysqli_query($conn, $sql);
+
+mysqli_close($conn);
+header("Location: adminmanagement.php");
+?>
--- manageuser.php
+++ manageuser.php
@@ -5,10 +5,10 @@ $conn = $db -> dbConnect();
 
 $id = mysqli_real_escape_string($conn, $_POST['ID']);
 
-if($_POST['Make Admin'] == 'Make Admin'){
+//if($_POST['Make Admin'] == 'Make Admin'){ if statement not working in current php version?
     $sql = "UPDATE users SET admin = 1 WHERE ID ='$id';";
     mysqli_query($conn,$sql);
-}
+//}
 mysqli_close($conn);
 header( "Location: adminmanagement.php");
 ?>
