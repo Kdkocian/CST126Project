e9c4832c32b04f94602e69fb0390b2d1bfd005b9
Kyle Kocian <krencent870@gmail.com>
2019-05-04 20:12:29 -0400

    added close statements

--- manageuser.php
+++ manageuser.php
@@ -1,20 +1,21 @@
 <?php
-include 'dp.php';
+include 'db.php';
 $db = new myfuncs();
-$conn = $db ->dbConnect();
+$conn = $db -> dbConnect();
+
 $id = mysqli_real_escape_string($conn, $_POST['ID']);
-echo "hello nathan";
+
 if(isset($_POST['Make Admin'])){
-$sql = "SELECT * FROM users WHERE ID = '$id'";
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_row($result);
-
-$adminstatus = false;
-$adminstatus = !$row['admin']; 
-
-$sql= "Update users SET admin = '$adminstatus' WHERE ID = '$id'";
-mysqli_query($conn, $sql);
-close($conn);
-header("Location: adminmanagement.php");
+    $sql = "SELECT * FROM users WHERE ID = '$id';";
+    $result = mysqli_query($conn, $sql);
+    
+    $row = mysqli_fetch_row($result);
+    
+    $adminstatus = false;
+    $adminstatus = !$row['admin'];
+    
+    $sql = "UPDATE users SET admin = '$adminstatus' WHERE ID ='$id';";
+    mysqli_query($conn,$sql);
 }
+mysqli_close($conn);
 ?>
