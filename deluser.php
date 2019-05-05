081f7a11f7a892be21ce2774b7a641d8f06be185
Kyle Kocian <krencent870@gmail.com>
2019-05-05 18:46:59 -0400

    fixing delete logic

--- deluser.php
+++ deluser.php
@@ -3,7 +3,7 @@ include 'db.php';
 $db = new myfuncs();
 $conn = $db -> dbConnect();
 
-$id = mysqli_real_escape_string($conn, $_POST['ID']);
+$id = mysqli_real_escape_string($conn, $_GET['ID']);
 
 $sql = "DELETE USERNAME FROM users WHERE ID = '$id'";
 mysqli_query($conn, $sql);
