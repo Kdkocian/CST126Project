2a4045d862494df87ac3bc4994fc53bba8a7036c
Kyle Kocian <krencent870@gmail.com>
2019-05-04 20:32:59 -0400

    changing manageuser logic

--- manageuser.php
+++ manageuser.php
@@ -7,14 +7,10 @@ $id = mysqli_real_escape_string($conn, $_POST['ID']);
 
 if(isset($_POST['Make Admin'])){
     $sql = "SELECT * FROM users WHERE ID = '$id';";
-    $result = mysqli_query($conn, $sql);
+mysqli_query($conn, $sql);
     
-    $row = mysqli_fetch_row($result);
     
-    $adminstatus = !$row['admin'];
-    $admin = (int)$adminstatus;
-    
-    $sql = "UPDATE users SET admin = '$admin' WHERE ID ='$id';";
+    $sql = "UPDATE users SET admin = 1 WHERE ID ='$id';";
     mysqli_query($conn,$sql);
 }
 mysqli_close($conn);
