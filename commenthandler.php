9f5eb60774dcca17ede36b5e5d3b8e5e6f56618b
Kyle Kocian <krencent870@gmail.com>
2019-05-14 16:05:56 -0400

    trying if isset to get data into the database

--- commenthandler.php
+++ commenthandler.php
@@ -2,7 +2,7 @@
 include 'db.php';
 $db = new myfuncs();
 $conn = $db -> dbConnect();
-
+if(isset($_POST('POST'))){
 $commentID = mysqli_real_escape_string($conn, $_GET['commentID']);
 $postID = mysqli_real_escape_string($conn, $_POST['pid']);
 $commentContent = mysqli_real_escape_string($conn, $_POST['comment_content']);
@@ -12,13 +12,14 @@ $userID = mysqli_real_escape_string($conn, $db->getUserID());
 $sql = "INSERT INTO comments (commentID, comment_content, postID, userID) VALUE ('$commentID', '$commentContent', '$postID', '$userID')";
 if($commentContent = "null"){
     echo " Please complete your comment.";
-    return;
-}
+} else {
 mysqli_query($conn, $sql);
+}
 if($db->getUseradmin()){
     header("Location: adminview.php");
 }else {
     header("Location: postview.php");
+}
 }
 mysqli_close($conn);
 ?>
