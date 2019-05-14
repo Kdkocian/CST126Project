5fefe3b0815743608c9177f92047707895b57133
Kyle Kocian <krencent870@gmail.com>
2019-05-14 01:44:35 -0400

    another try at getting info into the database

--- comment.php
+++ comment.php
@@ -8,7 +8,7 @@
 <form action = 'commenthandler.php' method = 'Post'>
 <?php
 echo "<input type='hidden' name = 'postID' value='".$_GET['pid']."'>";
-echo "<textarea name='commentcontent'>'.$commentcontent.'</textarea><br>";
+echo "<textarea name='commentcontent'>Write your comment here.</textarea><br>";
 echo "<input type = 'submit' name='submitform' value = 'Comment'>";
 ?>
 </form>;
