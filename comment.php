954293a366406f6162275bfb84940d8c020fdd17
Kyle Kocian <krencent870@gmail.com>
2019-05-13 22:20:25 -0400

    added quotes to comment.php

--- comment.php
+++ comment.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <?php
-echo "<form action = commenthandler.php method = Post>
+echo "<form action = 'commenthandler.php' method = 'Post'>
     <input type='hidden' name = 'userID' value='".$_GET['pid']."'>
     <input type='hidden' name = 'commentID' value='ID'>
     <textarea name='comment_content'></textarea><br>
