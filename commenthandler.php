1586e43779fa6c59239db2c2b05f99f2fee64358
Kyle Kocian <krencent870@gmail.com>
2019-05-13 21:55:42 -0400

    added varibles, seeing if varibles stick with other code

--- commenthandler.php
+++ commenthandler.php
@@ -3,7 +3,13 @@ include 'db.php';
 $db = new myfuncs();
 $conn = $db -> dbConnect();
 
-$sql = "INSERT into comments WHERE userID = ID";
+$commentID = strip_tags($_POST['pid']);
+$commentContent = strip_tags($_POST['comment_content']);
+
+$commentID = mysqli_real_escape_string($conn, $commentID);
+$commentContent = mysqli_real_escape_string($conn, $commentContent);
+
+$sql = "INSERT into comments (commentID, comment_content)  values = ($commentID, $commentContent)";
 
 mysqli_query($conn, $sql);
 
@@ -17,7 +23,7 @@ echo "<form action = commenthandler.php method = Post>
     <input type='hidden' name = 'userID' value='pid'>
     <input type='hidden' name = 'commentID' value='ID'>
     <textarea name='comment_content'></textarea><br>
-    <button type = 'submit' name='submit'>Comment</button>
+    <button type = 'submit' name='submit' value = 'POST'>Comment</button>
 </form>"
 ?>
 </body>
