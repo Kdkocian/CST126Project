8af43374bd55b72179079666764db72077a0bb31
Kyle Kocian <krencent870@gmail.com>
2019-05-13 22:15:01 -0400

    seperated html from commenthandler

--- comment.php
+++ comment.php
@@ -1,9 +1,17 @@
 <?php
-include 'db.php';
-$db = new myfuncs();
-$conn = $db -> dbConnect();
 
-$sql = "INSERT into comments WHERE userID = ID";
-
-mysqli_query($conn, $sql);
 ?>
+<html>
+<head>
+</head>
+<body>
+<?php
+echo "<form action = commenthandler.php method = Post>
+    <input type='hidden' name = 'userID' value='".$_GET['pid']."'>
+    <input type='hidden' name = 'commentID' value='ID'>
+    <textarea name='comment_content'></textarea><br>
+    <button type = 'submit' name='submit' value = 'POST'>Comment</button>
+</form>";
+?>
+</body>
+</html>
--- commenthandler.php
+++ commenthandler.php
@@ -12,19 +12,5 @@ $commentContent = mysqli_real_escape_string($conn, $commentContent);
 $sql = "INSERT into comments (commentID, comment_content)  values = ($commentID, $commentContent)";
 
 mysqli_query($conn, $sql);
-
+header("Location: postview.php");
 ?>
-<html>
-<head>
-</head>
-<body>
-<?php
-echo "<form action = commenthandler.php method = Post>
-    <input type='hidden' name = 'userID' value='pid'>
-    <input type='hidden' name = 'commentID' value='ID'>
-    <textarea name='comment_content'></textarea><br>
-    <button type = 'submit' name='submit' value = 'POST'>Comment</button>
-</form>"
-?>
-</body>
-</html>
