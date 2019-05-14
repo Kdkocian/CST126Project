b58dda1470e89bad3dc35598a6221531b68c04cf
Kyle Kocian <krencent870@gmail.com>
2019-05-13 23:43:16 -0400

    debugging

--- comment.php
+++ comment.php
@@ -8,7 +8,7 @@
 <?php
 echo "<form action = 'commenthandler.php' method = 'Post'>";
 echo "<input type='hidden' name = 'postID' value='".$_GET['pid']."'>";
-echo "<textarea name='comment_content'></textarea><br>";
+echo "<textarea name='commentcontent'></textarea><br>";
 echo "<input type = 'submit' name='submit' value = 'Comment'>";
 echo "</form>";
 ?>
--- commenthandler.php
+++ commenthandler.php
@@ -3,17 +3,17 @@ include 'db.php';
 $db = new myfuncs();
 $conn = $db -> dbConnect();
 
-
 $postID = mysqli_real_escape_string($conn, $_POST['pid']);
-$commentContent = mysqli_real_escape_string($conn, $_POST['comment_content']);
+$commentContent = mysqli_real_escape_string($conn, $_POST['commentcontent']);
 $userID = mysqli_real_escape_string($conn, $db->getUserID());
 
 $sql = "INSERT into comments (postID, comment_content, userID)  values ($postID, $commentContent, $userID)";
 
 mysqli_query($conn, $sql);
-if($db->getUseradmin()){
+/*if($db->getUseradmin()){
     header("Location: adminview.php");
 }else {
     header("Location: postview.php");
-}
+}*/
+mysqli_close($conn);
 ?>
