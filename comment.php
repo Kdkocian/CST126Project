c3235557c6f97b2492b1d6f4b68b2b65c084286f
Kyle Kocian <krencent870@gmail.com>
2019-05-13 22:38:21 -0400

    trying input instead of button

--- comment.php
+++ comment.php
@@ -9,7 +9,7 @@
 echo "<form action = 'commenthandler.php' method = 'Post'>
     <input type='hidden' name = 'postID' value='".$_GET['pid']."'>
     <textarea name='comment_content'></textarea><br>
-    <button type = 'submit' name='submit' value = 'POST'>Comment</button>
+    <input type = 'submit' name='submit' value = 'Comment'>
 </form>";
 ?>
 </body>
