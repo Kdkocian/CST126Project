658127715bd245eefe38b74f7be834d3c055dcd8
Kyle Kocian <krencent870@gmail.com>
2019-05-14 13:29:43 -0400

    added placeholder to comment.php text box

--- comment.php
+++ comment.php
@@ -8,7 +8,7 @@
 <form action = 'commenthandler.php' method = 'Post'>
 <?php
 echo "<input type='hidden' name = 'postID' value='".$_GET['pid']."'>";
-echo "<textarea name='comment_content'>Write your comment here.</textarea><br>";
+echo "<textarea placeholder = 'comment_content' name='comment_content'>Write your comment here.</textarea><br>";
 ?>
 <input type = 'submit' name='submitform' value = 'Comment'>
 </form>
