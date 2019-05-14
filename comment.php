53fd8b9c5e8ccb5f35730cacd8762efeb6f2c3d2
Kyle Kocian <krencent870@gmail.com>
2019-05-14 14:01:07 -0400

    removed php in the hmtl in comment.php

--- comment.php
+++ comment.php
@@ -6,10 +6,8 @@
 </head>
 <body>
 <form action = 'commenthandler.php' method = 'Post'>
-<?php
-echo "<input type='hidden' name = 'postID' value='".$_GET['pid']."'>";
-echo "<textarea placeholder = 'comment_content' name='comment_content'>Write your comment here.</textarea><br>";
-?>
+<input type='hidden' name = 'postID' value="$_GET['pid']">
+<textarea placeholder = 'comment_content' name='comment_content'>Write your comment here.</textarea><br>
 <input type = 'submit' name='submitform' value = 'Comment'>
 </form>
 </body>
--- commenthandler.php
+++ commenthandler.php
@@ -9,10 +9,9 @@ $commentContent = mysqli_real_escape_string($conn, $_POST['comment_content']);
 $userID = mysqli_real_escape_string($conn, $db->getUserID());
 
 
-$sql = "INSERT INTO comments (commentID, comment_content, postID, userID) values ('$commentID', '$commentContent', '$postID', '$userID')";
+$sql = "INSERT INTO comments (commentID, comment_content, postID, userID) VALUE ('$commentID', '$commentContent', '$postID', '$userID')";
 if($commentContent = "null"){
     echo " Please complete your comment.";
-    return;
 } else {
 mysqli_query($conn, $sql);
 }
