fba41d1e549f6eb7f3acd84cd53eb40df66c7de7
Kyle Kocian <krencent870@gmail.com>
2019-05-15 04:25:51 -0400

    refactored searchhandler

--- controllers/searchhandlerpage.php
+++ controllers/searchhandlerpage.php
@@ -5,13 +5,13 @@ $conn = $db->dbConnect();
 
 $searchresult = mysqli_real_escape_string($conn, $_GET['SearchBar']);
 
-$sql = "Select * from posts where post_content='$searchresult'";
+$sql = "Select * from posts where post_content LIKE '%$searchresult%'";
 
 $result = mysqli_query($conn, $sql);
 
 while($row = mysqli_fetch_array($result))
 {
-    echo $row['article_title']."<br.>".$row['post_content'];
+    echo $row['article_title']."<br/>".$row['post_content'];
 }
     echo "<a href='../views/searchpage.php'>Searchpage</a>";
 
