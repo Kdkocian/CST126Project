e1030f02469ea0eb331faf99fc17aa132aa78df9
Kyle Kocian <krencent870@gmail.com>
2019-05-15 04:12:10 -0400

    commented out the comments function

--- views/postview.php
+++ views/postview.php
@@ -19,11 +19,11 @@
                     
                     echo "<a href='view_post.php?pid=".$posts[$i][0]."'>".$posts[$i][2]."</a>";
                     echo "<p>".$posts[$i][3]."</p>";
-                    $postcomments = $db->getALLComments($posts[$i][0]);
+                    /*$postcomments = $db->getALLComments($posts[$i][0]);
                     for($i = 0; $i < count($postcomments); $i++)
                     {
                         echo "<p>".$postcomments[$i]."</p>";
-                    }
+                    }*/
                     echo "<a href='editPost.php?pid=".$posts[$i][0]."&title=".$posts[$i][2]."&content=".$posts[$i][3]."'>Edit</a>";
                     echo "<a href='comment.php?pid=".$posts[$i][0]."'>Comment</a>";
                 } 
@@ -42,11 +42,11 @@
                     echo "<a href='view_post.php?pid=".$posts[$i][0]."'>".$posts[$i][2]."</a>";
                     echo "<p>".$posts[$i][3]."</p>";
                     
-                    $postcomments = $db->getALLComments($posts[$i][0]);
+                    /*$postcomments = $db->getALLComments($posts[$i][0]);
                     for($i = 0; $i < count($postcomments); $i++)
                     {
                         echo "<p>".$postcomments[$i]."</p>";
-                    }
+                    }*/
                     
                     echo "<a href='../controllers/postManagement.php?pid=".$posts[$i][0]."&Delete=\"Delete\"'>Delete</a>";
                     echo "<a href='adminmanagement.php?pid=".$posts[$i][0]."'>Management</a>";
