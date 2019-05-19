166886e8511ff691a20390f5d2715784e5506dea
Kdkocian <krencent870@Gmail.com>
2019-05-19 17:50:15 -0500

    fixed the manage user page

--- views/adminManagement.php
+++ views/adminManagement.php
@@ -24,7 +24,7 @@
             echo '<td><input type = "hidden" name = "ID" value = "'.$user[$id][0].'">';
             echo '<input type="submit" name = "MakeAdmin" value="Make Admin" /></td>';
         echo "</form>";
-        echo "<form action = 'deluser.php' method = 'POST'>";
+        echo "<form action = '../controllers/userManagement.php' method = 'POST'>";
         echo '<td><input type = "hidden" name = "Delete" value = "'.$user[$id][0].'"><input type="submit" name = "Delete" value="Delete" />';
         echo "</form></tr>";
     }
